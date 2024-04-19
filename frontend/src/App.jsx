5234b5081865ef1398f4baae5e72641ad2856690
Amit Kumar Pati <121998394+sanuamit@users.noreply.github.com>
2024-04-19 12:42:21 +0530

    Add existing project files to Git

---
+++ frontend/src/App.jsx
@@ -0,0 +1,43 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url('path/to/your/image.jpg')`,
+        backgroundColor: "#947db8",
+        minHeight: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books/create" element={<CreateBook />} />
+          <Route path="/books/details/:id" element={<ShowBook />} />
+          <Route path="/books/edit/:id" element={<EditBook />} />
+          <Route path="/books/delete/:id" element={<DeleteBook />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
